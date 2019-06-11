type passed to kl divergence was incorrect. remedied more consistent approach required there ito types

tests/test_measures.py
@@ -22,5 +22,5 @@ class TestKLDivergence(unittest.TestCase):
         """
         x_axis = np.arange(-10, 10, 0.01)
         # Mean = 0, SD = 2.
-        dist = scipy.stats.norm.pdf(x_axis, 0, 2)
+        dist = pd.DataFrame(scipy.stats.norm.pdf(x_axis, 0, 2))
         self.assertAlmostEqual(measures.kullback_leibler_divergence(dist, dist), 0.0)